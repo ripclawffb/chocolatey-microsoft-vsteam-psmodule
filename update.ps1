fd6e2d300195d97512de4d9cd7a35f97e8d1ef0c
Asif Shaikh <ripclaw_ffb@hotmail.com>
2021-04-21 22:49:09 -0700

    Fix update script

--- update.ps1
+++ update.ps1
@@ -2,12 +2,16 @@ import-module au
 
 $releases = 'https://www.powershellgallery.com/packages/VSTeam'
 
+function global:au_SearchReplace {
+    @{}
+}
+
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
 
     $match = $download_page.Content | Select-String -Pattern '(href=.*VSTeam.*">)'
     $url = $match.Matches[0].value
-    
+
     $version  = $url -split '[%2F]|" target="' | Select-Object -Last 1 -Skip 1
 
     @{
@@ -16,7 +20,7 @@ function global:au_GetLatest {
 }
 
 try {
-    update -ChecksumFor all
+    update -ChecksumFor none
 } catch {
     $ignore = 'Unable to connect to the remote server'
     if ($_ -match $ignore) { Write-Host $ignore; 'ignore' }  else { throw $_ }
